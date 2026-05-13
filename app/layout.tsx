b52612b0d0def366a253e077e1cf6e36878da446
Harry Nguyen <harry.nguyen22@outlook.com.au>
2026-05-13 14:35:38 +1000

    Add profile pic
    Style components for home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Link from "next/link";
 
 const montserrat = Montserrat({
-  variable: '--font-monserrat',
+  variable: '--font-montserrat',
   subsets: ['latin'],
 })
 
@@ -21,15 +21,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${montserrat.variable} ${poppins.variable} h-full antialiased`}>
+    <html lang="en" className={`${montserrat.variable} ${poppins.variable} h-full antialiased px-6`}>
       <body className="min-h-full flex flex-col mx-auto w-full max-w-screen-xl">
-        <header className="p-4 flex justify-between">
+        <header className="py-8 flex justify-between">
           <p>Logo</p>
           <nav>
             <Link href='/projects'>Projects</Link>
           </nav>
         </header>
         {children}
+        <footer className="py-8 flex justify-between">© Harry Nguyen 2026</footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 export default function Home() {
   return <>
-    <main id="about-me">
-      <div className="container">
-        <div id="profile-pic" role="img" aria-label="Profile pic"></div>
-        <article id="about-me-blurb">
-          <h1>About me</h1>
+    <main id="about-me" className="h-full container py-8 flex flex-col justify-center self-center flex-grow max-w-screen-lg">
+      <div className="flex flex-col md:flex-row gap-8 lg:gap-16 items-center">
+        <Image className="rounded-full border-foreground border-4" src="/profile.png" alt="Picture of the site author" width={300} height={300} loading="eager" />
+        <article className="text-justify flex flex-col gap-4" id="about-me-blurb">
+          <h1 className="font-bold text-5xl">About me</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ut minima quaerat quasi sint nemo unde
             praesentium id reiciendis quam. Sequi porro similique dignissimos soluta neque, recusandae possimus eligendi
             quae.</p>
