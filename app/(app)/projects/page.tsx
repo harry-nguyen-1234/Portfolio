2de1837924208c619c2f7b986ddb2627cf041ab4
Harry Nguyen <harry.nguyen22@outlook.com.au>
2026-05-15 16:08:29 +1000

    Switch project from postgres to sqlite

--- app/(app)/projects/page.tsx
+++ app/(app)/projects/page.tsx
@@ -1,4 +1,12 @@
-export default function Projects() {
+import { getPayload } from 'payload'
+import config from '@payload-config'
+
+
+export default async function Projects() {
+    const payload = await getPayload({ config });
+    const projects = (await payload.find({ collection: 'projects' })).docs;
+    console.log(projects);
+
     return <>
         <main id="about-me" className="h-full container py-8 flex flex-col self-center grow">
             <h1 className="font-bold text-5xl text-center">Projects</h1>
